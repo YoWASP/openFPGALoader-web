a39d540f58d62070db827ed6e3ee002ef9d7e4c4
Catherine <whitequark@whitequark.org>
2023-12-25 03:22:06 +0000

    Use upstream version for npm package version.

--- npmjs/prepare.mjs
+++ npmjs/prepare.mjs
@@ -1,10 +1,13 @@
 import { execFileSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 
+const gitDescribe = execFileSync('git', ['-C', '../openFPGALoader-src', 'describe'], {'encoding': 'utf-8'});
+const [, major, minor, patch, node] = gitDescribe.match(/^v(\d+).(\d+).(\d+)(?:-(\d+)-)?/);
+
 const revCount = execFileSync('git', ['rev-list', 'HEAD'], {encoding: 'utf-8'}).split('\n').length - 1;
 const packageJSON = JSON.parse(readFileSync('package-in.json', {encoding: 'utf-8'}));
 
-let version = `0.0.0-experimental${revCount - 1}`;
+const version = `${major}.${minor}.${patch}-${node ?? '0'}.${revCount}`;
 console.log(`version ${version}`);
 
 packageJSON.version = version;
