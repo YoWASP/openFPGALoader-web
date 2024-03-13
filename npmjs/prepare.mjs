d5ebaf27d1382ac47a1483154927c27e61a00ccc
Catherine <whitequark@whitequark.org>
2024-03-13 05:09:10 +0000

    [autorelease] Fix package version determination.

--- npmjs/prepare.mjs
+++ npmjs/prepare.mjs
@@ -1,7 +1,7 @@
 import { execFileSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 
-const gitDescribe = execFileSync('git', ['-C', '../openFPGALoader-src', 'describe'], {'encoding': 'utf-8'});
+const gitDescribe = execFileSync('git', ['-C', '../openFPGALoader-src', 'describe', '--tags', '--match', 'v*'], {'encoding': 'utf-8'});
 const [, major, minor, patch, node] = gitDescribe.match(/^v(\d+).(\d+).(\d+)(?:-(\d+)-)?/);
 
 const revCount = execFileSync('git', ['rev-list', 'HEAD'], {encoding: 'utf-8'}).split('\n').length - 1;
